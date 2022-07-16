452e15a92843a013ceb52acddea51391d2d34024
Graeme Lockley <graeme.lockley@gmail.com>
2022-07-16 13:24:54 +0000

    fix: lint error

--- scripts/lint-factorial-calc-deno.sh
+++ scripts/lint-factorial-calc-deno.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd "$SCRIPT_DIR"/../containers/factorial-calc
+cd "$SCRIPT_DIR"/../containers/factorial-calc || exit 1
 
 deno lint || exit 1
 deno fmt --check
