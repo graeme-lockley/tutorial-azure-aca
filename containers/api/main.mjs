267dade7947e1f3b9ceedfb176c2899f1b14079d
Graeme Lockley <graeme.lockley@gmail.com>
2022-07-17 06:49:44 +0000

    feature: dump environment

--- containers/api/main.mjs
+++ containers/api/main.mjs
@@ -5,7 +5,11 @@ const app = new Koa();
 const router = new Router();
 
 router.get('/hello', (ctx) => {
-  ctx.body = 'Hello World';
+  ctx.body = `Hello World at ${process.env.FACTORIAL_URN}`;
+});
+
+router.get('/env', (ctx) => {
+  ctx.body = JSON.stringify(process.env, null, 2);
 });
 
 app.use(router.routes());
