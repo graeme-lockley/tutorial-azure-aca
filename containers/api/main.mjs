c21f755fcb3317a6f0f106bbfa89986d3f70ecb3
Graeme Lockley <graeme.lockley@gmail.com>
2022-07-17 07:41:39 +0000

    feature: route API through to factorial-calc

--- containers/api/main.mjs
+++ containers/api/main.mjs
@@ -1,15 +1,46 @@
 import Koa from 'koa';
 import Router from 'koa-router';
+import https from 'https';
 
 const app = new Koa();
 const router = new Router();
 
 router.get('/hello', (ctx) => {
-  ctx.body = `Hello World at ${process.env.FACTORIAL_URN}`;
+  ctx.body = 'Hello World';
 });
 
 router.get('/env', (ctx) => {
   ctx.body = JSON.stringify(process.env, null, 2);
+});
+
+router.get('/api/v1/factorial/:n([0-9]+)', async (ctx) => {
+  const n = ctx.params.n;
+
+  const factorialURN = process.env.FACTORIAL_URN ??
+    'tutacafact.victoriouscoast-832e0370.westeurope.azurecontainerapps.io';
+
+  const options = {
+    hostname: factorialURN,
+    port: 443,
+    path: `/api/v1/factorial?${n}`,
+    method: 'GET',
+  };
+
+  const result = await new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      res.on('data', (d) => {
+        resolve(Buffer.from(d).toString());
+      });
+    });
+
+    req.on('error', (error) => {
+      reject(error);
+    });
+
+    req.end();
+  });
+
+  ctx.body = result;
 });
 
 app.use(router.routes());
