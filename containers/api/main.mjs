2a569fb3b2b3f29be67a44121ab75ad060e6dbeb
Graeme Lockley <graeme.lockley@gmail.com>
2022-07-23 07:02:38 +0000

    refactor: clean up a little

--- containers/api/main.mjs
+++ containers/api/main.mjs
@@ -6,67 +6,44 @@ import http from 'http';
 const app = new Koa();
 const router = new Router();
 
+const ackermannURN = process.env.ACKERMANN_URN ?? 'localhost';
+const ackermannPort = process.env.ACKERMANN_PORT ?? '443';
+
+const factorialURN = process.env.FACTORIAL_URN ?? 'localhost';
+const factorialPort = process.env.FACTORIAL_PORT ?? '443';
+
 router.get('/hello', (ctx) => {
   ctx.body = 'Hello World';
-});
+});    
 
 router.get('/env', (ctx) => {
   ctx.body = JSON.stringify(process.env, null, 2);
-});
+});    
 
 router.get('/api/v1/ackermann/:m([0-9]+)/:n([0-9]+)', async (ctx) => {
   const m = ctx.params.m;
   const n = ctx.params.n;
 
-  const [ackermannURN, ackermannPort] = [
-    process.env.ACKERMANN_URN ?? 'localhost',
-    process.env.ACKERMANN_PORT ?? '443',
-  ];
-
-  const options = {
-    hostname: ackermannURN,
-    port: ackermannPort,
-    path: `/api/v1/ackermann/${m}/${n}`,
-    method: 'GET',
-  };
-
-  const result = await new Promise((resolve, reject) => {
-    const protocol = ackermannURN.indexOf('.') === -1 ? http : https;
-
-    const req = protocol.request(options, (res) => {
-      res.on('data', (d) => {
-        resolve(Buffer.from(d).toString());
-      });
-    });
-
-    req.on('error', (error) => {
-      reject(error);
-    });
-
-    req.end();
-  });
-
-  ctx.body = result;
-});
+  await callRestAPI(ctx, ackermannURN, ackermannPort, `/api/v1/ackermann/${m}/${n}`);
+});  
 
 router.get('/api/v1/factorial/:n([0-9]+)', async (ctx) => {
   const n = ctx.params.n;
 
-  const [factorialURN, factorialPort] = [
-    process.env.FACTORIAL_URN ?? 'localhost',
-    process.env.FACTORIAL_PORT ?? '443',
-  ];
+  await callRestAPI(ctx, factorialURN, factorialPort, `/api/v1/factorial?${n}`);
+});
+
+const callRestAPI = async (ctx, urn, port, path) => {
+  const protocol = urn.indexOf('.') === -1 ? http : https;
 
   const options = {
-    hostname: factorialURN,
-    port: factorialPort,
-    path: `/api/v1/factorial?${n}`,
+    hostname: urn,
+    port: port,
+    path: path,
     method: 'GET',
   };
 
   const result = await new Promise((resolve, reject) => {
-    const protocol = factorialURN.indexOf('.') === -1 ? http : https;
-
     const req = protocol.request(options, (res) => {
       res.on('data', (d) => {
         resolve(Buffer.from(d).toString());
@@ -81,7 +58,7 @@ router.get('/api/v1/factorial/:n([0-9]+)', async (ctx) => {
   });
 
   ctx.body = result;
-});
+}
 
 app.use(router.routes());
 
