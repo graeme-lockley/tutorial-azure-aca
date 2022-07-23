726ab6ea48cf35fc20778c130cfc2a46772bc092
Graeme Lockley <graeme.lockley@gmail.com>
2022-07-23 06:14:53 +0000

    feature: wire in ackermann

--- containers/api/main.mjs
+++ containers/api/main.mjs
@@ -14,6 +14,41 @@ router.get('/env', (ctx) => {
   ctx.body = JSON.stringify(process.env, null, 2);
 });
 
+router.get('/api/v1/ackermann/:m([0-9]+)/:n([0-9]+)', async (ctx) => {
+  const m = ctx.params.m;
+  const n = ctx.params.n;
+
+  const [ackermannURN, ackermannPort] = [
+    process.env.ACKERMANN_URN ?? 'localhost',
+    process.env.ACKERMANN_PORT ?? '443',
+  ];
+
+  const options = {
+    hostname: ackermannURN,
+    port: ackermannPort,
+    path: `/api/v1/ackermann/${m}/${n}`,
+    method: 'GET',
+  };
+
+  const result = await new Promise((resolve, reject) => {
+    const protocol = ackermannURN.indexOf('.') === -1 ? http : https;
+
+    const req = protocol.request(options, (res) => {
+      res.on('data', (d) => {
+        resolve(Buffer.from(d).toString());
+      });
+    });
+
+    req.on('error', (error) => {
+      reject(error);
+    });
+
+    req.end();
+  });
+
+  ctx.body = result;
+});
+
 router.get('/api/v1/factorial/:n([0-9]+)', async (ctx) => {
   const n = ctx.params.n;
 
