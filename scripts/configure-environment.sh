b002490af61cb4868e37f667ed6924157661feb5
Graeme Lockley <graeme.lockley@gmail.com>
2022-07-26 06:14:33 +0200

    refactor: move infra into src/

--- scripts/configure-environment.sh
+++ scripts/configure-environment.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd "$SCRIPT_DIR"/../infra || exit 1
+cd "$SCRIPT_DIR"/../src/infra || exit 1
 "$SCRIPT_DIR"/infra-runner.sh up
