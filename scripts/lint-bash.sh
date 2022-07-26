37f0dfcbea0f3557147522ee4e190741096d3db3
Graeme Lockley <graeme.lockley@gmail.com>
2022-07-26 06:18:02 +0200

    fix: infra src

--- scripts/lint-bash.sh
+++ scripts/lint-bash.sh
@@ -7,6 +7,6 @@ shellcheck -e SC1090,SC1091 "$PROJECT_HOME"/.tasks/* || exit 1
 shellcheck -e SC1090,SC1091 "$PROJECT_HOME"/scripts/*.sh || exit 1
 
 shellcheck -e SC1090,SC1091 \
-    "$PROJECT_HOME"/infra/*.sh \
-    "$PROJECT_HOME"/infra/*.up \
-    "$PROJECT_HOME"/infra/*.down
+    "$PROJECT_HOME"/src/infra/*.sh \
+    "$PROJECT_HOME"/src/infra/*.up \
+    "$PROJECT_HOME"/src/infra/*.down
