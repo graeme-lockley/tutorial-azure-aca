74310461d15859b355722ee806c1b32643da221f
Graeme Lockley <graeme.lockley@gmail.com>
2022-07-30 06:43:08 +0000

    chore: wire in a lint task

--- scripts/lint-bash.sh
+++ scripts/lint-bash.sh
@@ -6,6 +6,8 @@ shellcheck -e SC1090,SC1091 "$PROJECT_HOME"/.tasks/* || exit 1
 
 shellcheck -e SC1090,SC1091 "$PROJECT_HOME"/scripts/*.sh || exit 1
 
+shellcheck -e SC1090,SC1091 "$PROJECT_HOME"/tasks/* || exit 1
+
 shellcheck -e SC1090,SC1091 \
     "$PROJECT_HOME"/src/infra/*.sh \
     "$PROJECT_HOME"/src/infra/*.up \
