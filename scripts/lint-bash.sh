fa1de47da3a93b9b70976d817ee5acd8a8f1c754
Graeme Lockley <graeme.lockley@gmail.com>
2022-07-30 06:47:11 +0000

    fix: bash lint

--- scripts/lint-bash.sh
+++ scripts/lint-bash.sh
@@ -2,7 +2,9 @@
 
 PROJECT_HOME=$( "$( dirname "$0" )/../.tasks/project-home" ) || exit 1
 
-shellcheck -e SC1090,SC1091 "$PROJECT_HOME"/.tasks/* || exit 1
+shellcheck -e SC1090,SC1091 \
+    "$PROJECT_HOME"/.tasks/is-executable \
+    "$PROJECT_HOME"/.tasks/project-home || exit 1
 
 shellcheck -e SC1090,SC1091 "$PROJECT_HOME"/scripts/*.sh || exit 1
 
