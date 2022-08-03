693a027f7c8cd59a9e957c667491597cff9edf97
Graeme Lockley <graeme.lockley@gmail.com>
2022-08-03 04:05:12 +0000

    feature: add rudimentary java lint

--- containers/ackermann-calc/src/main/java/za/co/no9/ackermann/Controller.java
+++ containers/ackermann-calc/src/main/java/za/co/no9/ackermann/Controller.java
@@ -10,19 +10,22 @@ import java.util.Map;
 
 @RestController
 public final class Controller {
-    private static final HashMap<BigInteger, HashMap<BigInteger, BigInteger>> cache = new HashMap<>();
+    private static final HashMap<BigInteger, HashMap<BigInteger, BigInteger>> CACHE = 
+        new HashMap<>();
 
     @GetMapping("/api/v1/ackermann/{m}/{n}")
     public BigInteger ackermann(@PathVariable("m") BigInteger m, @PathVariable("n") BigInteger n) {
-        if (m.equals(BigInteger.ZERO))
+        if (m.equals(BigInteger.ZERO)) {
             return n.add(BigInteger.ONE);
-        else {
+        } else {
             BigInteger v = get(m, n);
 
             if (v == null) {
                 v = n.equals(BigInteger.ZERO)
                     ? ackermann(m.subtract(BigInteger.ONE), BigInteger.ONE)
-                    : ackermann(m.subtract(BigInteger.ONE), ackermann(m, n.subtract(BigInteger.ONE)));
+                    : ackermann(
+                        m.subtract(BigInteger.ONE), 
+                        ackermann(m, n.subtract(BigInteger.ONE)));
             }
 
             put(m, n, v);
@@ -32,12 +35,12 @@ public final class Controller {
     }
 
     private BigInteger get(BigInteger m, BigInteger n) {
-        Map<BigInteger, BigInteger> t = cache.get(m);
+        Map<BigInteger, BigInteger> t = CACHE.get(m);
 
         return (t == null) ? null : t.get(n);
     }
 
     private void put(BigInteger m, BigInteger n, BigInteger v) {
-        cache.computeIfAbsent(m, k -> new HashMap<>()).put(n, v);
+        CACHE.computeIfAbsent(m, k -> new HashMap<>()).put(n, v);
     }
 }
--- containers/ackermann-calc/src/test/java/za/co/no9/ackermann/ControllerTest.java
+++ containers/ackermann-calc/src/test/java/za/co/no9/ackermann/ControllerTest.java
@@ -12,6 +12,9 @@ class ControllerTest {
         Controller greeting = new Controller();
 
         assertEquals(greeting.ackermann(BigInteger.ZERO, BigInteger.ZERO), BigInteger.ONE);
-        assertEquals(greeting.ackermann(BigInteger.valueOf(2), BigInteger.ONE), BigInteger.valueOf(5));
+        
+        assertEquals(
+            greeting.ackermann(BigInteger.valueOf(2), BigInteger.ONE), 
+            BigInteger.valueOf(5));
     }
 }
