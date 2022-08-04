317e1799437915011ea575b79b02464c35352051
Graeme Lockley <graeme.lockley@gmail.com>
2022-08-04 04:18:53 +0000

    chore: create a docker task

--- scripts/ackermann-calc-docker-image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-cd "$SCRIPT_DIR"/../containers/ackermann-calc || exit 1
-
-docker build . -t graemel/tutorial-azure-aca-ackermann-calc
